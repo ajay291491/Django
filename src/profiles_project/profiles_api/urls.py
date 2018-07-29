21687bc1a54fa13150b48a09ae826eb767ed71a5
ajay291491@gmail.com <ajay291491@gmail.com>
2018-07-28 21:33:33 -0400

    updated src directory

--- src/profiles_project/profiles_api/urls.py
+++ src/profiles_project/profiles_api/urls.py
@@ -1,6 +1,9 @@
-from django.conf.urls import url
-from . import views
+from django.conf.urls import url	# This module is to handle the url
+from . import views			# We are importing views.py in our apps base directory here 
 
 urlpatterns = [ 
-    url(r'^hello-view/', views.HelloApiView.as_view()),
+    
+    # Using the url module, we are defining the api view 'hello-view/'
+    # hello-view will display the get method defined under the HelloApiView class under the 'views.py' as the view
+    url(r'^hello-view/', views.HelloApiView.as_view()),	
     ]
--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -9,15 +9,23 @@ class HelloApiView(APIView):
     """ Test API View """
     
     def get(self, request, format=None):
-    	""" Returns a list of APIView Features """
+        """ Returns a list of APIView Features """
 
-    	an_apiview = [
-    		'Uses HTTP methods as functions (get, post, patch, put , delete)',
-    		'It is similar to traditional Django view',
-    		'Gives you the most control over your application logic', 
-    		'Its manually mapped to URLs']
+        an_apiview = [
+            'Uses HTTP methods as functions (get, post, patch, put , delete)',
+            'It is similar to traditional Django view',
+            'Gives you the most control over your application logic', 
+            'Its manually mapped to URLs'
+        ]
+
+        family_details = {
+            'Father'   : 'Ajayaghosh V L',
+            'Mother'   : 'Aparna A',
+            'Daughter' : 'Vaiga Shanti A',
+            'Son'      : 'Rishi Krishna A',
+        }
 
 
-	# response has to be always send it a dictionary format, for that we will associate our list with a key as seen below 
-    	return Response({'Message' : 'Hello, welcome to Ajay\'s first api endpoint', 'an_apiview': an_apiview })
+        # response has to be always send it a dictionary format, for that we will associate our list with a key as seen below 
+        return Response({'Message' : 'Hello, welcome to Ajay\'s first api endpoint', 'an_apiview': an_apiview, 'family_details' : family_details })
 
